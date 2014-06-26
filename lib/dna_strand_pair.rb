fce7fac609955c1916ced3a7a783157925c74d04
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-26 10:12:09 -0600

    Completed three versions of the hamming calculator

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,50 @@
 class DnaStrandPair
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1.to_s
+    @strand_2 = strand_2.to_s
+    @hamming = 0
+  end
+
+  # calculated with slice & recursion
+  def recursive_hamming_distance
+    compare_1 = @strand_1.slice!(0)
+    compare_2 = @strand_2.slice!(0)
+
+    if compare_1 == nil || compare_2 == nil
+      return @hamming
+    end
+
+    if compare_1 != compare_2
+      @hamming += 1
+    end
+
+    recursive_hamming_distance
+  end
+
+  # calculated with each_with_index
+  def index_hamming_distance
+    compare_1 = @strand_1.split(//)
+    compare_2 = @strand_2.split(//)
+
+    compare_1.each_with_index do |char, ndx|
+      if compare_2[ndx] != char && compare_2[ndx] != nil
+        @hamming += 1
+      end
+    end
+    @hamming
+  end
+
+  # calculated with find_all
+  def hamming_distance
+    compare_1 = @strand_1.split(//)
+    compare_2 = @strand_2.split(//)
+
+    iter = -1
+    array_mismatch = compare_1.find_all do |letter|
+      iter += 1
+      compare_2[iter] != letter && compare_2[iter] != nil
+    end
+    array_mismatch.length
+  end
 
 end
